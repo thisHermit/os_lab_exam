d1dbbab5a33f621fe7f97e104ff329ac57a8c3ac
Bashar Jaan Khan <411706@student.nitandhra.ac.in>
2019-11-06 12:42:40 +0530

    Added Politics

--- signals.cpp
+++ signals.cpp
@@ -1,21 +1,36 @@
 #include <iostream>
 #include <csignal>
+#include <unistd.h>
 
 using namespace std;
 
 void signal_handler(int signal_num) {
-	cout << "The intterupt signal is (" << signal_num << "). \n";
+	cout << "Modi says (" << signal_num << "). \n";
 
 	// terminate program
 	exit(signal_num);
 }
 
+void signal_handler1(int signal_num) {
+	cout << "Rahul says (" << signal_num << "). \n";
+
+	// terminate program
+	exit(signal_num);
+
+}
+
 int main() {
-	signal(SIGABRT, signal_handler);
+	signal(SIGINT, signal_handler);
+	signal(SIGSEGV, signal_handler1);
 	// register signal SIGABRT and signal handler
-	//
-	
-	while (true) 
+	int count = 0;
+	while (true) { 
+		// 5 second loop, Ctrl+C to see the handler
+		if ( count == 5)
+			raise(SIGSEGV);
 		cout << "Hello Bashar..." << endl;
+		sleep(1);
+		count++;
+	}
 	return 0;
 }
